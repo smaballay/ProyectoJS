8b68f9f067854446555f1fe33fafd8982183cdcc
smaballay <so.aballay5@gmail.com>
2022-11-05 22:13:10 -0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -23,9 +23,9 @@ function iniciarSesion(){
                 login="bloqueada";
                 break;
             } else if (intentos>1){
-                alert("Datos ingresados incorrectos. Le quedan "+intentos+" intentos. \nLuego su cuenta quedará bloqueada.");
+                alert(`Datos ingresados incorrectos. Le quedan ${intentos} intentos. \nLuego su cuenta quedará bloqueada.`);
             } else{
-                alert("Datos ingresados incorrectos. Le queda "+intentos+" intento. \nLuego su cuenta quedará bloqueada.");
+                alert(`Datos ingresados incorrectos. Le queda ${intentos} intento. \nLuego su cuenta quedará bloqueada.`);
             }
             let salida = prompt("Si desea salir ingrese ESC, sino presione aceptar");
             if (salida.toUpperCase()=="ESC"){
@@ -57,70 +57,68 @@ BBDD.push(new Cabania(5, "Cabaña Alpina de Madera", 10000, 4));
 
 //Funcion para reservar una cabaña dependiendo la cantidad de personas y noches, validando primero que el usuario este logeado
 function reservar(){
-    const cabaniasOp = [];
     let registrado = iniciarSesion();
 
     if(registrado==true){
         let noches = Number(prompt("Ingrese la cantidad de noches que desea alquilar"));
         let personas = Number(prompt("Ingrese la cantidad de personas (máximo 8):"));
-        let cabaniaSel;
-
+        
         if (noches>0 && personas>0 && personas<=8){
-            for (let ob of BBDD){
-                if (ob.capacidadMax >= personas){
-                    cabaniasOp.push(ob);
+            //Nuevo array para almacenar las cabañas que cumplen con la capacidad de personas ingresadas 
+            const cabaniasOp = BBDD.filter((cabania)=> cabania.capacidadMax >= personas);
+
+            //Variable para almacenar en texto el array y mostrar junto con el prompt
+            let textOp;
+            cabaniasOp.forEach((cabania)=> textOp+=`\n${cabania.id} - ${cabania.nombre} $${cabania.precio}`);
+
+            //Variable para guardar la opción elegida por el usuario y buscar la cabaña seleccionada
+            let cabaniaId = Number(prompt(`Ingrese el número de cabaña a elegir: ${textOp}`));
+            const cabaniaSel = cabaniasOp.find(cabania => cabania.id === cabaniaId);
+
+            //Primero se verifica que se halla encontrado la opción elegida entre las opciones y se procede al cálculo del precio final según la cantidad de noches ingresadas
+            if (cabaniaSel!==undefined){
+                let precio;
+                let ok;
+                if (noches>=7){
+                    precio = cabaniaSel.precio*noches*0.9;
+                    ok = prompt(`FELICITACIONES! OBTUVO UN 10% POR RESERVAR MÁS DE 7 NOCHES! \nSu estadía de ${noches} noches en ${cabaniaSel.nombre} tiene un costo total de $${precio} final.\n\nIngrese "ok" para confirmar la reserva`);
+                } else{
+                    precio = cabaniaSel.precio*noches;
+                    ok = prompt(`Su estadía de ${noches} noches en ${cabaniaSel.nombre} tiene un costo total de $${precio} final.\n\nIngrese "ok" para confirmar la reserva`);
+                }  
+
+                if (ok.toLocaleLowerCase() === "ok"){
+                    registrarReserva(personas, cabaniaSel, noches, precio);
+                } else{
+                    alert("Gracias por usar nuestro servicio");
                 }
+
+            } else{
+                alert("Opción de cabaña inexistente");
             }
-            cabaniaSel = Number(prompt("Ingrese el número de cabaña a elegir: " + mostrarCabanias(cabaniasOp)));
         } else {
             alert("Cantidad de noches o personas incorrectos. Inténtelo nuevamente.");
         }
     } 
+} 
+
+class Reserva{
+    constructor(id, titular, cantidadPersonas, cabaniaSel, noches, precio){
+        this.idReserva = id;   
+        this.titularReserva = titular;
+        this.cantidadPersonas = cantidadPersonas;
+        this.cabaniaSeleccionada = cabaniaSel;
+        this.noches = noches;
+        this.precioFinal = precio;
+    }
 }
 
-function mostrarCabanias(cabaniasOp){
-    let text = "";
+const BBDDReservas = [];
+let idReservas = 0;
 
-    cabaniasOp.forEach((cabania)=> text+="\n" + cabania.id + " - "+ cabania.nombre + " $"+cabania.precio);
-
-    return text;
-}
-
-//Funcion para calcular el valor de una estadía dependiendo la cantidad de noches y cabaña seleccionada
-function selCabania(cabania, noches){
-    let precio;
-    let nomCabania;
-
-    switch(cabania){
-        case 1:
-            nomCabania = "Cabaña Azul";
-            precio = noches * 15000;
-            break;
-        case 2:
-            nomCabania = "Cabaña Naranja"
-            precio = noches * 12500;
-            break;
-        case 3:
-            nomCabania = "Cabaña Verde"
-            precio = noches * 12500;
-            break;
-        case 4:
-            nomCabania = "Cabaña Alpina de Piedra"
-            precio = noches * 10000;
-            break;
-        case 5:
-            nomCabania = "Cabaña Alpina de Madera"
-            precio = noches * 10000;
-            break;
-        default:
-            alert("Número de cabaña inexistente.");
-    }
-
-    if (precio>0){
-        if (noches>=7){
-            alert("FELICITACIONES! OBTUVO UN 10% POR RESERVAR MÁS DE 7 NOCHES! \nSu estadía de " + noches + " noches en " + nomCabania + " tiene un costo total de $" + precio*0.9 + " final.");
-        } else{
-            alert("Su estadía de " + noches + " noches en " + nomCabania + " tiene un costo total de $" + precio + " final.");
-        }    
-    }
+function registrarReserva(personas, cabaniaSel, noches, precio){
+    const titular = prompt("Ingrese el nombre y apellido del titular de la reserva:");
+    BBDDReservas.push(new Reserva(idReservas, titular, personas, cabaniaSel.nombre, noches, precio));
+    console.log(BBDDReservas[idReservas++]);
+    alert("Reserva exitosa! Muchas gracias por confiar en nosotros!");
 }
